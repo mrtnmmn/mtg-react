7188a256b500b5a377bf2cb0fab69bb0e648dc38
mrtnmmn <mrtn.lmba@gmail.com>
2022-03-27 16:06:29 +0200

    added refresh button

--- src/Components/RandomCard.js
+++ src/Components/RandomCard.js
@@ -1,19 +1,26 @@
 import { useEffect, useState } from 'react';
 import '../Css/RandomCard.css'
+import logo from '../Assets/logoMtg.webp'
 
 function RandomCard() {
 
     const [card, setCard] = useState()
+    const [loadedImg, setLoadedImg] = useState(false)
 
     const api = 'https://api.scryfall.com/cards/random'
 
     useEffect(() => {
 
+        getCard()
+
+    } ,[] );
+
+    function getCard() {
         fetch(api)
         .then((res) => res.json())
         .then((data) => setCard(data))
-
-    } ,[] );
+        .then(console.log(card))
+    }
 
     return (  
         <div className='mainDivRandom'>
@@ -24,6 +31,7 @@ function RandomCard() {
                 </div> 
                 : 'Loading...'}
             </div>
+            <button onClick={getCard}>Refresh card</button>
         </div>
     );
 }
