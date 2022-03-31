affbdff96f43bae43ef7b0a73caf1a94e0665440
mrtnmmn <mrtn.lmba@gmail.com>
2022-03-31 03:00:06 +0200

    added main page

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import RandomCard from './Components/RandomCard';
 import CardFinder from './Components/CardFinder';
 
 import {Route, Routes} from "react-router-dom";
+import MainPage from './Components/MainPages';
 
 function App() {
   return (
     <div className="mainDiv">
       <NavBar/>
       <Routes>
+        <Route path='/' element={<MainPage/>}/>
         <Route path='/random' element = {<RandomCard/>}/>
         <Route path='/finder' element = {<CardFinder/>}/>
       </Routes>
---
+++ src/Components/MainPages.js
@@ -0,0 +1,21 @@
+import '../Css/MainPage.css'
+
+function MainPage() {
+    return (  
+        <div className="mainPageDiv">
+            <div className="mtgInfo">
+                <div>
+                    mtg 
+                </div>
+                <div>
+                    mtg
+                </div>
+            </div>
+            <div className="devInfo">
+                <h1 className='devName'>@mrtnmmn</h1>
+            </div>
+        </div>
+    );
+}
+
+export default MainPage;
