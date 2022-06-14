3820fba079bb2ae83e423085a014a9eb808b8d78
mrtnmmn <mrtn.lmba@gmail.com>
2022-06-15 00:32:13 +0200

    deleted unnecessary import

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 import '../Css/Login.css'
-import useLogin from './Hooks/useLogin'
 
 function Login(props) {
 
